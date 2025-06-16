70d22f1f2f70d96c29814431fa4573207a74c814
Gabriel Henrique Salm de Borba <gabrielborba0903@gmail.com>
2025-06-16 16:36:33 -0300

    Delete script3.js

--- script3.js
+++
@@ -1,7 +0,0 @@
-var num1 = Number(prompt("Informe um número"))
-var num2 = Number(prompt("Informe um outro número"))
-var diferenca, nummaior, nummenor
-
-diferenca = num1 - num2
-
-alert("diferença:" + diferenca)
